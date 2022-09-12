91e787914cd8914fc6f431e37a39582f15884b6a
simonebonifazi <simonbonifraz@gmail.com>
2022-09-12 17:10:36 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+/*
+Oggi pomeriggio provate ad immaginare quali sono le classi necessarie per creare uno shop online con le seguenti caratteristiche.
+L'e-commerce vende prodotti per gli animali.
+I prodotti saranno oltre al cibo, anche giochi, cucce, etc.
+L'utente potrà sia comprare i prodotti senza registrarsi, oppure iscriversi e ricevere il 20% di sconto su tutti i prodotti.
+Il pagamento avviene con la carta di credito, che non deve essere scaduta.
+BONUS:
+Alcuni prodotti (es. antipulci) avranno la caratteristica che saranno disponibili solo in un periodo particolare (es. da maggio ad agosto).
+NOTE:
+Lo scopo di questo esercizio è farvi ragionare su quali classi utilizzare, quali proprietà e metodi dovrebbero avere e come sono relazionate tra loro anche in termini di ereditarietà.
+Cercate di fare molta analisi, magari facendo uno schemino delle classi e delle loro proprietà e metodi su draw.io e solo dopo passate al codice. Meglio un'analisi
+fatta bene che del codice scritto male.
+Non preoccupatevi se non riuscite a finirlo, purchè sia fatto bene fin dove arrivate.
+Buon lavoro!
+*/
+?>
