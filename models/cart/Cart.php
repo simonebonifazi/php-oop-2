1530b7dfee2ac586586645ba0cb6de02776cc4e3
simonebonifazi <simonbonifraz@gmail.com>
2022-09-14 08:57:02 +0200

    cart added

--- models/cart/Cart.php
+++ models/cart/Cart.php
@@ -1,1 +1,30 @@
 //!!carrello
+<?php
+
+require_once '../products/Product.php';
+
+class Cart
+{
+    private $products = [];
+
+    public function getProductList()
+    {
+        return $this->products;
+    }
+
+    public function getTotal()
+    {
+        $total = 0;
+        
+        foreach($this->products as $product){
+            $total += $product->getPrice();
+        }
+        return $total;
+    }
+
+    public function addProduct($product)
+    {
+        if(!($product instanceof Product)) return false;
+        $this->products[] = $product;
+    }
+}
